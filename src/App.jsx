df83ed2028176f3aecc281ab9851d02ad273ab3f
Partha <rajashwin@gmail.com>
2026-07-25 14:31:52 +0530

    feat: Added the Footer.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, useLocation } from "react-router-dom"
 import Home from "./pages/Home"
 import Navbar from "./components/Navbar"
 import LoginPage from "./pages/LoginPage"
+import Footer from './components/Footer'
 
 function App() {
   const location = useLocation();
@@ -18,6 +19,7 @@ function App() {
       <Route path="/" element={<Home />}/>
       <Route path="/login" element={<LoginPage/>}/>
     </Routes>
+    {showNavbar && <Footer/ >}
   </>
   )
 }
