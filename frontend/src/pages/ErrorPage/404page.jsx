7b740f1a8c4f1d255c64edd0a37263ac091abf40
nithyanandharaj607-ship-it <nithyanandharaj607@gmail.com>
2026-07-28 18:00:59 +0530

    add404errorpage

---
+++ frontend/src/pages/ErrorPage/404page.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./404page.css";
+
+function ErrorPage() {
+  return (
+    <div className="error-container">
+      <h1 className="error-code">404</h1>
+      <h2 className="error-title">Page Not Found</h2>
+      <p className="error-message">
+        Sorry, the page you’re looking for doesn’t exist or has been moved.
+      </p>
+      <Link to="/" className="home-link">
+        ⬅ Back to Home
+      </Link>
+    </div>
+  );
+}
+
+export default ErrorPage;
