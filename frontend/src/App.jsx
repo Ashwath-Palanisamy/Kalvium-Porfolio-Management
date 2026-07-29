9fdcd816fb245255eb4109a545ccc14a6912409f
Partha <rajashwin@gmail.com>
2026-07-29 08:47:05 +0530

    feat:Add the editprofile.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,16 +7,6 @@ import IndividualStudentPortfolio from "./pages/IndividualStudentPortfolio";
 import Students from "./pages/Students";
 import About from "./pages/About/About";
 import EditProfile from "./pages/EditProfile/EditProfile";
-import { Routes, Route, useLocation } from "react-router-dom"
-import Home from "./pages/Home"
-import Navbar from "./components/Navbar"
-import LoginPage from "./pages/LoginPage"
-import Footer from './components/Footer'
-import IndividualStudentPortfolio from "./pages/IndividualStudentPortfolio"
-import Students from "./pages/Students"
-import About from "./pages/About/About"
-import ErrorPage from "./pages/ErrorPage/404page"; 
-import EditStudent from "./pages/EditStudent"
 
 function App() {
   const location = useLocation();
@@ -44,20 +34,6 @@ function App() {
       {showFooter && <Footer />}
     </>
   );
-    {showNavbar && <Navbar />}
-    
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/login" element={<LoginPage/>}/>
-      <Route path='/students' element={<Students/>}/>
-      <Route path='/manage' element={<IndividualStudentPortfolio/>}/>
-      <Route path="/about" element={<About />}/>
-      <Route path="/profile" element={<EditStudent />}></Route>
-      <Route path="*" element={<ErrorPage />} />
-    </Routes>
-    {showNavbar && <Footer/ >}
-  </>
-  )
 }
 
 export default App;
