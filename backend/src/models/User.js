1359f91aa341d4c687ef7e8a88326d9560712303
dhinesh-babu-7 <dhineshbabu8th@gmail.com>
2026-07-29 19:31:23 +0530

    refactor: backend/src/models/user.js

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -21,7 +21,43 @@ const userSchema = new mongoose.Schema(
 
         picture: {
             type: String,
-        }  
+        },
+
+        personalEmail : {
+            type: String,
+            default: "",
+        },
+
+        squadId : {
+            type: String,
+            default : "",
+        },
+
+        bio : {
+            type: String,
+            default : ""
+        },
+
+        githubId : {
+            type: String,
+            default : ""
+        },
+
+        linkedInId : {
+            type: String,
+            default: ""
+        },
+
+        LeetCodeId : {
+            type: String,
+            default : ""
+        },
+
+        resume : {
+            type: String,
+            default: ""
+        }
+
     },
     {
         timestamps: true,
