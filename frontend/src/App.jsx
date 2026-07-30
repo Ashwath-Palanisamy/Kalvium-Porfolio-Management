865a21d83a4261eebf6cf3d24ad1a633cebb5b97
Partha1107 <rajashwin1107@gmail.com>
2026-07-30 11:24:49 +0530

    feat:Added the 404 pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import IndividualStudentPortfolio from "./pages/IndividualStudentPortfolio";
 import Students from "./pages/Students";
 import About from "./pages/About/About";
 import EditProfile from "./pages/studentdashboard/EditProfile";
+import ErrorPage from "./pages/ErrorPage/404page";
 
 function App() {
   const location = useLocation();
@@ -29,6 +30,8 @@ function App() {
         <Route path="/profile/:id" element={<IndividualStudentPortfolio />} />
         <Route path="/about" element={<About />} />
         <Route path="/dashboard" element={<EditProfile />} />
+        
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
 
       {showFooter && <Footer />}
