2c01bc47774046c57e0252daeee6cb72a1c62bfc
Partha1107 <rajashwin1107@gmail.com>
2026-07-30 13:03:02 +0530

    fix:fixed the Oauth gate

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import { Routes, Route, useLocation } from "react-router-dom";
-import Home from "./pages/Home.jsx";
-import Navbar from "./components/Navbar.jsx";
-import LoginPage from "./pages/LoginPage.jsx";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import LoginPage from "./pages/LoginPage";
 import Footer from "./components/Footer";
 import IndividualStudentPortfolio from "./pages/IndividualStudentPortfolio";
 import Students from "./pages/Students";
 import About from "./pages/About/About";
 import EditProfile from "./pages/studentdashboard/EditProfile";
 import ErrorPage from "./pages/ErrorPage/404page";
-import AuthGate from "./components/AuthGate";
+
 
 function App() {
   const location = useLocation();
@@ -30,13 +30,7 @@ function App() {
         <Route path="/students" element={<Students />} />
         <Route path="/profile/:id" element={<IndividualStudentPortfolio />} />
         <Route path="/about" element={<About />} />
-        <Route path="/dashboard"
-          element={
-            <AuthGate>
-              <EditProfile />
-            </AuthGate>
-          }
-        />
+        <Route path="/dashboard" element={<EditProfile />} />
         
         <Route path="*" element={<ErrorPage />} />
       </Routes>
--- frontend/src/components/AuthGate.jsx
+++ frontend/src/components/AuthGate.jsx
@@ -1,6 +1,6 @@
 import { Navigate } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { supabase } from "../../../backend/src/config/supabase";
+import { supabase } from "../lib/supabase"; // Update the path if yours is different
 
 function AuthGate({ children }) {
     const [loading, setLoading] = useState(true);
