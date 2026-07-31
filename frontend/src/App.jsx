670de557aa297778701caa57a3f9aaa1fb4a2f77
Ashwath <ashwakausi@outlook.com>
2026-07-31 20:47:40 +0530

    fix: navbar-not-showing-in-home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,6 @@ import MentorDashboard from "./pages/MentorDashboard/mentordashboard.jsx";
 function App() {
   const location = useLocation();
   
-  const isMentorDashboard = location.pathname === "/";
   const showNavbar = !isMentorDashboard && location.pathname !== "/login" && !location.pathname.startsWith("/dashboard");
   const showFooter = !isMentorDashboard && location.pathname !== "/login" && !location.pathname.startsWith("/dashboard");
 
