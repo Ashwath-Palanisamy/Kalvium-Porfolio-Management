1e301d4db09f5385c4bcc78b32fedb1d775451cc
Ashwath <ashwakausi@outlook.com>
2026-08-10 19:42:55 +0530

    Feat: Workflow file for cron job and leetcode leaderboard

--- backend/src/config/supabase.js
+++ backend/src/config/supabase.js
@@ -5,12 +5,15 @@ dotenv.config();
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_ANON_KEY;
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY;
 
-if (!supabaseUrl || !supabaseKey) {
-  throw new Error("Missing Supabase URL or Anon Key in environment variables.");
+if (!supabaseUrl || !supabaseKey || !supabaseServiceKey) {
+  throw new Error("Missing required Supabase environment variables (URL, ANON_KEY, or SERVICE_KEY).");
 }
 
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
+
 
 export function createAuthedSupabaseClient(token) {
   return createClient(supabaseUrl, supabaseKey, {
@@ -25,3 +28,10 @@ export function createAuthedSupabaseClient(token) {
     }
   });
 }
+
+export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceKey, {
+  auth: {
+    autoRefreshToken: false,
+    persistSession: false
+  }
+});
