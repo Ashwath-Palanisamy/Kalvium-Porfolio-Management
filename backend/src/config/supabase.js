b1099a33a2305893f61237dbe69227608b8ce3d3
Ashwath <ashwakausi@outlook.com>
2026-08-11 08:14:04 +0530

    Add warning for missing service key

--- backend/src/config/supabase.js
+++ backend/src/config/supabase.js
@@ -7,8 +7,8 @@ const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_ANON_KEY;
 const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY;
 
-if (!supabaseUrl || !supabaseKey || !supabaseServiceKey) {
-  throw new Error("Missing required Supabase environment variables (URL, ANON_KEY, or SERVICE_KEY).");
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Missing required Supabase environment variables (URL, ANON_KEY).");
 }
 
 
@@ -29,6 +29,7 @@ export function createAuthedSupabaseClient(token) {
   });
 }
 
+if (!supabaseServiceKey) throw new console.warn("Supabase service key missing - it is optional as it is only required for cron job only at the moment.")
 export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceKey, {
   auth: {
     autoRefreshToken: false,
